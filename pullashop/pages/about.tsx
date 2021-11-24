0266830ac511836e93882c375cdeb9ab7f733476
Ville.Kähärä <kahara.ville@gmail.com>
2021-11-24 16:23:18 +0200

    Add About page

---
+++ pullashop/pages/about.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Meta from "../components/Meta";
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <Meta title="About"/>
+            <h1>About us</h1>
+            <p>PullaShop is the best place to browse pastry and drink information and checkout the most sweet combinations of these pastries and drinks</p>
+        </div>
+    )
+}
+export default About;
