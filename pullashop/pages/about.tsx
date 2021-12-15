0375ee8e3f49484e54bd7a668cf13803b8cd5ae1
Ville.Kähärä <kahara.ville@gmail.com>
2021-12-15 09:22:19 +0200

    Add: localisation to About us page

--- pullashop/pages/about.tsx
+++ pullashop/pages/about.tsx
@@ -1,12 +1,25 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 import Meta from "../components/Meta";
+import LoadLocalization from "../localization/LoadLocalization";
+import Locale from "../localization/Locale";
+import ResourceText from "../localization/ResourceText";
 
 const About: NextPage = () => {
+    const [translationData, setTranslationData] = useState<ResourceText>();
+    useEffect(() => {
+        const getTranslations = async () => {
+            const translations = await LoadLocalization(Locale.FI);
+            setTranslationData(translations);
+        }
+        getTranslations();
+    },[])
+    
     return (
         <div>
             <Meta title="About"/>
-            <h1>About us</h1>
-            <p>PullaShop is the best place to browse pastry and drink information and checkout the most sweet combinations of these pastries and drinks</p>
+            <h1>{translationData?.aboutUsTitle}</h1>
+            <p>{translationData?.introduction}</p>
         </div>
     )
 }
