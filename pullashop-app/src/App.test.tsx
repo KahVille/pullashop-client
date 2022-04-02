6ea090a5a83d6a07105f5d7d4812ab41568a35a4
Ville Kähärä <kahara.ville@gmail.com>
2022-04-02 09:39:31 +0300

    Add App renders home page test

--- pullashop-app/src/App.test.tsx
+++ pullashop-app/src/App.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+describe("App", () => {
+  test("renders homepage", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+    // Expect landing page
+    expect(getByText(/Welcome to the PullaShop/i)).toBeInTheDocument();
+  });
 });
