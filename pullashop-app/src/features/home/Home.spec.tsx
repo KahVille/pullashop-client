bf41fc2a2af66fede9d88f585163968a56f75d27
Ville Kähärä <kahara.ville@gmail.com>
2022-04-02 09:48:15 +0300

    Add coming soon content test

--- pullashop-app/src/features/home/Home.spec.tsx
+++ pullashop-app/src/features/home/Home.spec.tsx
@@ -6,7 +6,11 @@ describe("home page", () => {
     render(<Home />);
 
     const appText = screen.getByText("Welcome to the PullaShop");
+    const underDevelopmentText = screen.getByText(
+      "Coming soon! Pullashop is currently under development."
+    );
 
     expect(appText).toBeDefined();
+    expect(underDevelopmentText).toBeDefined();
   });
 });
